0bde5f065452bae854ef0c57171afa035bbb6a6c
Pedro Henrique <contatophss11@gmail.com>
2022-08-30 14:59:56 -0300

    Cadastrar despesa concluido

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+//! Classe Despesa
 class Despesa {
     constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano
@@ -9,12 +9,88 @@ class Despesa {
         this.descricao = descricao 
         this.valor = valor
     }
+
+    validarDados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
 }
 
 
-//! Cadastar despesa
+/**------------------------------ */
+
+
+//! Classe para Banco de Dados e criação do id para cadastrar despesa
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    getProximoId () {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar (d) {
+        
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
+
+/**------------------------------ */
+
+
+
+//! Função que controla o estilo do modal de acordo com validação positiva / negativa
+const modal = document.getElementById('modalRegistroDespesa')
+const tituloModal = document.getElementById('exampleModalLabel')
+const divModal = document.getElementById('divTituloModal')
+const textoModal = document.getElementById('textoModal')
+const botaoModal = document.getElementById('botaoModal')
+
+
+function modalPositivo() {
+    tituloModal.innerText = 'Registro incluido com sucesso'
+    textoModal.innerText = 'Sucesso na gravação, a despesa foi cadastrada com sucesso!'
+    divModal.classList.remove('text-danger')
+    divModal.classList.add('text-success')
+    botaoModal.classList.remove('btn-danger')
+    botaoModal.classList.add('btn-success')
+}
+
+
+function modalNegativo() {
+    tituloModal.innerText = 'Erro na inclusão do registro'
+    textoModal.innerText = 'Erro na gravação, existem campos obrigatórios que não foram preenchidos corretamente!'
+    divModal.classList.remove('text-success')
+    divModal.classList.add('text-danger')
+    botaoModal.classList.remove('btn-success')
+    botaoModal.classList.add('btn-danger')
+}
+
+
+/**------------------------------ */
+
+
+//! Função Cadastar despesa
 const botaoCadastro = document.getElementById('cad-but')
 function cadastrarDespesa () {
+
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
     let dia = document.getElementById('dia')
@@ -25,11 +101,24 @@ function cadastrarDespesa () {
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-
-    console.log(despesa)
+    if(despesa.validarDados()) {
+        bd.gravar(despesa)
+        //dialog de sucesso
+        modalPositivo()
+        $('#modalRegistraDespesa').modal('show')
+        
+    } else {
+        //dialog erro
+        modalNegativo()
+        $('#modalRegistraDespesa').modal('show')
+        
+    }
+    
 }
-
 botaoCadastro.addEventListener('click', cadastrarDespesa)
+
+
+/**------------------------------ */
 
 
 //! 

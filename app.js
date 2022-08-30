ff0cfc82ce69db31472dda18b99f47b21aff5375
Pedro Henrique <contatophss11@gmail.com>
2022-08-30 16:30:26 -0300

    intrudução filtro consulta

--- app.js
+++ app.js
@@ -47,6 +47,39 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+
+
+    recuperarTodosRegistros () {
+
+        //array despesas
+
+        let despesas = Array()
+
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas do localStorage
+        for (let i = 1; i <= id; i++) {
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+
+            //existe a possibilidade de haver indices que foram pulados ou removidos
+            //nesse caso pular o indice
+            if(despesa === null) {
+                continue
+            }
+
+
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
+
+    pesquisar(despesa) {
+        console.log(despesa)
+    }
 }
 
 let bd = new Bd()
@@ -87,6 +120,9 @@ function modalNegativo() {
 /**------------------------------ */
 
 
+//! Função Limpar os inputs Após validação positiva da despesa
+
+
 //! Função Cadastar despesa
 const botaoCadastro = document.getElementById('cad-but')
 function cadastrarDespesa () {
@@ -106,7 +142,13 @@ function cadastrarDespesa () {
         //dialog de sucesso
         modalPositivo()
         $('#modalRegistraDespesa').modal('show')
-        
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+   
     } else {
         //dialog erro
         modalNegativo()
@@ -115,10 +157,91 @@ function cadastrarDespesa () {
     }
     
 }
-botaoCadastro.addEventListener('click', cadastrarDespesa)
+botaoCadastro.addEventListener('click', cadastrarDespesa);
 
 
 /**------------------------------ */
 
 
-//! 
+//! Carregar Lista das Despesas
+
+
+// const bodyConsulta = document.getElementById('bodyConsulta')
+
+
+function carregarListaDespesas () {
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+
+    // selecionado o elenmento tbody da tabela
+    let listaDepesas = document.getElementById('listaDespesas')
+
+
+
+    /**
+            <tr>
+                <td>15/03/2018</td>
+                <td>Alimentação</td>
+                <td>Comprar do mês</td>
+                <td>444.75</td>
+            </tr> 
+    */
+
+
+
+
+    //percorrer despesas listando DESPESA dinamicamente
+    despesas.forEach(function(d) {
+        // console.log(d)
+
+
+        //criando do <TR>
+        let linha = listaDepesas.insertRow()
+
+        //inserir as colunas <TD>
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+
+        //ajusta o tipo
+
+        switch(parseInt(d.tipo)) {
+            case 1 : d.tipo = 'Alimentação'
+                break
+            case 2 : d.tipo = 'Educação'
+                break
+            case 3 : d.tipo = 'Lazer'
+                break
+            case 4 : d.tipo = 'Saúde'
+                break
+            case 5 : d.tipo = 'Transporte'
+                break
+        
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
+}
+
+
+
+//! Função pesquisar as despeas (filtro)
+// const botaoPesquisa = document.getElementById('btnPesquisa')
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    
+
+    bd.pesquisar(despesa)
+}
+
